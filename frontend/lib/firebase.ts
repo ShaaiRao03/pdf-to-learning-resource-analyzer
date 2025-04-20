5e597251d9682c31f5a311ec9deff7f6963ff67a
ShaaiRao03 <shaairao03@gmail.com>
2025-04-21 06:10:12 +0800

    final commit

--- frontend/lib/firebase.ts
+++ frontend/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDy_T3OkHAdPElIqNBR1fZIo8ii_yUaAqo",
-  authDomain: "einstein-ai-prod-13811.firebaseapp.com",
-  projectId: "einstein-ai-prod-13811",
-  storageBucket: "einstein-ai-prod-13811.firebasestorage.app",
-  messagingSenderId: "623603689151",
-  appId: "1:623603689151:web:28f4a336b123d36151080a",
-  measurementId: "G-JKB54EN11G"
+  apiKey: "AIzaSyDSyfmi-vjFBPjVkbNZ9cd1arn_mf-NH5Q",
+  authDomain: "einstein-ai-prod.firebaseapp.com",
+  projectId: "einstein-ai-prod",
+  storageBucket: "einstein-ai-prod.firebasestorage.app",
+  messagingSenderId: "218136169622",
+  appId: "1:218136169622:web:f15c6e2ecbff0a1c95c6b7",
+  measurementId: "G-1MYQGC8EL3"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
